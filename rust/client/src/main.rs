cdbb589982d7277f496326bcead648aabfb5cda1
primardj <b00168148@mytudublin.ie>
2024-02-04 21:53:05 +0000

    create a simple connection server mono thread.

--- rust/client/src/main.rs
+++ rust/client/src/main.rs
@@ -1,22 +1,38 @@
 
-use std::io::prelude::*;
+use std::io::{BufReader, BufRead, Write};
 use std::net::TcpStream;
 
 
 
-fn main() -> std::io::Result<()> {
+fn main() {
     
-    let stream = TcpStream::connect("127.0.0.1:7878");
+    if let Ok(stream) = TcpStream::connect("127.0.0.1:7878") {
+        println!("Could connect to server!");
+        handle_connection(stream)
 
-    // To sent a packet
-    let _ = stream?.write(&[1]);
+
+    } else {
+        println!("Couldn't connect to socket. Is the server is up ?");
+    }
+
+}
+
+
+fn handle_connection(mut stream: TcpStream) {
+    // To make a new line, enter \r\n, to signal the End of String, \r\n\r\n
+    let message = "I am a client ;)\r\nalllala\r\n\r\n";
+
+    stream.write_all(message.as_bytes()).unwrap();
     println!("Packet sent!");
 
-    // To receive a packet
-    //stream.read(&mut [0; 128]);
-    //println!("Packet received");
+    let contents: Vec <_> = BufReader::new(&mut stream)
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
-    Ok(())
+    println!("Answer: {:#?}", contents);
 }
+
 
 // Reference https://doc.rust-lang.org/std/net/struct.TcpStream.html
--- rust/server/src/main.rs
+++ rust/server/src/main.rs
@@ -1,13 +1,34 @@
-use std::net::TcpListener;
+use std::net::{TcpListener,TcpStream};
+use std::io::{BufReader,BufRead,Write};
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Packet received!");
+        println!("A new connection!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let contents: Vec <_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    
+    println!("Request: {:#?}'", contents);
+    
+    // To make a new line, enter \r\n, to signal the End of String, \r\n\r\n
+    let response = "Okay, do nothing for now o/\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+
+    
 }
 
 
