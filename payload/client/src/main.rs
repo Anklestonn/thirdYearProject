f5187de73a5795a7e6d920044ae50f86fb1990fa
primardj <b00168148@mytudublin.ie>
2024-03-18 22:09:49 +0000

    ask the server what to do every few seconds

--- payload/client/src/main.rs
+++ payload/client/src/main.rs
@@ -2,27 +2,34 @@
 use openssl::ssl::{SslMethod, SslConnector, SslStream, SslVerifyMode};
 use std::io::{BufReader, BufRead, Write};
 use std::net::TcpStream;
+use std::process::Command;
 
 
 
 fn main() {
+
+    loop {
     
-    let builder = SslConnector::builder(SslMethod::tls());
-    let mut builder = builder.expect("dk");
-    builder.set_verify(SslVerifyMode::empty());
-    let connector = builder.build();
+        let builder = SslConnector::builder(SslMethod::tls());
+        let mut builder = builder.expect("dk");
+        builder.set_verify(SslVerifyMode::empty());
+        let connector = builder.build();
 
-    if let Ok(stream) = TcpStream::connect("127.0.0.1:7878") {
-        
-        let stream = connector.connect("127.0.0.1",stream).unwrap();
+        if let Ok(stream) = TcpStream::connect("127.0.0.1:7878") {
+            
+            let stream = connector.connect("127.0.0.1",stream).unwrap();
 
-        println!("Could connect to server!");
-        
-        handle_connection(stream);
+            println!("Could connect to server!");
+            
+            handle_connection(stream);
 
 
-    } else {
-        println!("Couldn't connect to socket. Is the server is up ?");
+        } else {
+            println!("Couldn't connect to socket. Is the server is up ?");
+        }
+
+        let _ = Command::new("sleep")
+            .args(["2"]).output();
     }
 
 }
