01df93cad694b1a55fb4659ab92ca2b0a1d487b2
primardj <b00168148@mytudublin.ie>
2024-04-10 17:09:40 +0100

    write order when client receive it.

--- payload/client/src/main.rs
+++ payload/client/src/main.rs
@@ -4,6 +4,9 @@ use std::io::{BufReader, BufRead, Write};
 use std::net::TcpStream;
 use std::process::Command;
 
+use std::fs;
+// use std::io::prelude::*;
+
 
 
 fn main() {
@@ -29,7 +32,7 @@ fn main() {
         }
 
         let _ = Command::new("sleep")
-            .args(["2"]).output();
+            .args(["5"]).output();
     }
 
 }
@@ -51,7 +54,47 @@ fn handle_connection(mut stream: SslStream<TcpStream>) {
 
     println!("Received: {:#?}", contents);
 
+    let content_as_string = make_one_string(contents);
+
+    write_order(content_as_string);
+
     println!("End of connection \n");
+}
+
+fn make_one_string(vec_string: Vec<String>) -> String {
+    let mut ret = String::new();
+    for chaine in vec_string.iter() {
+        if chaine != "" {
+            ret.push_str(&chaine);
+            ret.push('\n');
+        }
+
+    }
+
+    return ret
+}
+
+fn write_order(contents: String) -> u32{
+    // return an error code of 1 in case file couldn't be write.
+    // if everything good, return 0.
+    
+    let order_option = fs::OpenOptions::new().write(true).create(true).open("order");
+    let mut order = match order_option {
+        Ok(file) => file,
+        Err(error) => {
+            dbg!(error);
+            println!("impossible to write contents");
+            return 1;
+        },
+    };
+
+    let contents_bytes = contents.as_bytes();
+    let _ = order.write_all(&contents_bytes);
+
+    return 0;
+
+    
+    
 }
 
 
