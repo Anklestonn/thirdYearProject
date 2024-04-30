ac87671ccac6b6a0e0a9b18c89fe40f5530f7232
otitoko <tbkopera@proton.me>
2024-04-30 18:57:14 +0200

    working on args feature. broken atm

--- payload/client/src/args.rs
+++ payload/client/src/args.rs
@@ -2,22 +2,24 @@ use std::env::args;
 use std::net::{SocketAddr, Ipv4Addr, IpAddr};
 use std::string::String;
 use std::fs::File;
+use std::io::Read;
 
-pub fn get_args()-> (IpAddr, SocketAddr, SocketAddr){
+pub fn get_args()-> Result<(IpAddr, SocketAddr, SocketAddr),&'static str>{
     let args: Vec<String> = args().collect();
 
     if args.len() == 0{
+
         let mut file = File::open("../conf/default_args")?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
 
-        let vector: Vec<&str> = file.split(' ').collect();
+        let vector: Vec<&str> = contents.split(' ').collect::<Vec<_>>();
         let ip_vector: Vec<&str> = vector[1].split('.').collect();
 
-    let ip_addr_1: u8 = ip_vector[0].parse().expect("Conf file error");
-    let ip_addr_2: u8 = ip_vector[1].parse().expect("Conf file error");
-    let ip_addr_3: u8 = ip_vector[2].parse().expect("Conf file error");
-    let ip_addr_4: u8 = ip_vector[3].parse().expect("Conf file error");
+        let ip_addr_1: u8 = ip_vector[0].parse().expect("Conf file error");
+        let ip_addr_2: u8 = ip_vector[1].parse().expect("Conf file error");
+        let ip_addr_3: u8 = ip_vector[2].parse().expect("Conf file error");
+        let ip_addr_4: u8 = ip_vector[3].parse().expect("Conf file error");
 
         let fs_port: u16 = vector[2].parse().expect("Conf file error");
         let cc_port: u16 = vector[3].parse().expect("Conf file error");
@@ -29,7 +31,7 @@ pub fn get_args()-> (IpAddr, SocketAddr, SocketAddr){
         let cc_sock_addr = SocketAddr::new(ip_addr, cc_port);
 
 
-        (ip_addr, fs_sock_addr, cc_sock_addr)
+        Ok((ip_addr, fs_sock_addr, cc_sock_addr))
     } else{
         let ip_addr: Vec<&str> = args[1].split('.').collect();
 
@@ -45,7 +47,7 @@ pub fn get_args()-> (IpAddr, SocketAddr, SocketAddr){
     let fs_sock_addr = SocketAddr::new(ip_addr,fs_port);
     let cc_sock_addr = SocketAddr::new(ip_addr,cc_port);
 
-    (ip_addr, fs_sock_addr, cc_sock_addr)
+        Ok((ip_addr, fs_sock_addr, cc_sock_addr))
     }
 
 
