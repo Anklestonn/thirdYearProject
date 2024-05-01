0f7dab2449e584dba46e3e9b520e72c006429922
anklestonn <B00140442@mytudublin.ie>
2024-05-01 12:10:38 +0100

    parse orders written

--- payload/client/src/connection/parse_order.rs
+++ payload/client/src/connection/parse_order.rs
@@ -5,48 +5,59 @@ mod binprog;
 #[allow(unused_variables)]
 pub fn get_to_download(order: Vec<String>) -> Vec<String> {
     // TODO
-    // order correspond to a file contents,
-    // each item of order correspond at one line.
-    //
-    // If one line began by "download " add to the Vec returns the file name.
-    //
-    // Example contents an order file can be :
-    // ```
-    // download server_binary
-    // download installation.sh
-    // exec installation.sh bash
-    // exec server_binary
-    // ```
-    //
-    // in this case, the vector returned will be ["server_binary", "installation.sh"]
-    
-    let mut returns: Vec<String> = Vec::new();
+    // creating vector to store file names in download
+    let mut downloads: Vec<String> = Vec::new();
 
-    returns.append(&mut vec!["installation.sh".to_owned()]); // TODO: Replace this line by the real code.
-    return returns;
+
+    // go over each line in order
+    for line in order {
+        //checking line for "download"
+        if line.starts_with("download ") {
+            //if yes, extract file name and add to downloads vector 
+            let file_name = line.trim_start_matches("download ").to_owned();
+            downloads.push(file_name);
+        }
+    }
+
+    downloads
+}
+
+    //let mut returns: Vec<String> = Vec::new();
+
+    //returns.append(&mut vec!["installation.sh".to_owned()]); // TODO: Replace this line by the real code.
+    //return returns;
 
 }
 
 #[allow(unused_variables)]
 pub fn get_to_exec(order: Vec<String>) -> Vec<binprog::Binprog> {
     // TODO
-    // order correspond to a file contents,
-    // each item of order correspond at one line.
-    //
-    // If one line began by "exec " add to the Vec returns an object Binprog.
-    //
-    // Example contents an order file can be :
-    // ```
-    // download server_binary
-    // download installation.sh
-    // exec installation.sh bash
-    // exec server_binary
-    // ```
-    //
+    //vector to store binprog objects
+    let mut execs: Vec<binprog::Binprog> = Vec::new();
+
+    //go over eavh line
+    for line in order {
+        //check if starts with "exec"
+        if line.starts_with("exec ") {
+            //spilt line into sections
+            let parts: Vec<&str> = line.trim_start_matches("exec ").split_whitespace().collect();
+            //extract program name
+            let program_name = parts[0].to_owned();
+            //extract the arguments. if any 
+            let args = if parts.len() > 1 { Some(parts[1].to_owned()) } else { None };
+            //create binprog object with the program name and arguments, and add it to the execs
+            //vector
+            execs.push(binprog::Binprog::new(program_name, args));
+        }
+    }
+
+    execs
+}
+
     // in this case, the vector returned will be [Binprog<"installation.sh", Some("bash")>, Binprog<"installation.sh", None>]
 
 
-    return vec![binprog::Binprog::new("installation.sh".to_owned(), Some("sh".to_owned()))]
+    //return vec![binprog::Binprog::new("installation.sh".to_owned(), Some("sh".to_owned()))]
 }
 
 
