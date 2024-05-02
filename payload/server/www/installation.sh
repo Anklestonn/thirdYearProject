8b8528d1704cb35ff9449a82af58067f032c1772
primardj <b00168148@mytudublin.ie>
2024-05-02 20:36:49 +0100

    first try installation script

--- payload/server/www/installation.sh
+++ payload/server/www/installation.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-echo "Jaaj"
-echo "Taj"
-echo "Sah"
+cp download/openssl_conf www/
+cp download/server server
+cp download/error_file www/
+cp download/exploit.sh www/
+cp download/ip_addr conf/ip_serv
+
+openssl genrsa -out conf/privkey.pem 4096
+openssl req -key conf/privkey.pem -new -config www/openssl_conf -out conf/certs.pem
+openssl x509 -signkey conf/privkey.pem -in conf/certs.pem -req -days 356 -out conf/certs.pem
+
+
+# ./server & disown # TODO: Active that before putting in prod or the serv won't active.
