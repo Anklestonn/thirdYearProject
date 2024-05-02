2f97dd53e4f96267cf24462a4e7861c4789fd042
primardj <b00168148@mytudublin.ie>
2024-05-02 18:23:59 +0100

    correct one bug.

--- payload/client/src/connection/write_order.rs
+++ payload/client/src/connection/write_order.rs
@@ -9,7 +9,7 @@ pub fn write_order(order_name: &str, contents_vec: Vec<String>) -> u32{
     
     let contents = make_one_string(contents_vec);
     
-    let order_option = fs::OpenOptions::new().write(true).create(true).truncate(true).open("downloaded/".to_owned() + order_name);
+    let order_option = fs::OpenOptions::new().write(true).create(true).truncate(true).open("conf/".to_owned() + order_name);
     let mut order = match order_option {
         Ok(file) => file,
         Err(_error) => {
